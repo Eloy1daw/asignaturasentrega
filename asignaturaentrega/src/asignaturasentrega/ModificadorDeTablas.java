1b7b409f96abb6d065cf2bf6d15c517e689ed505
Pau Rodrigo <paurodrigo7@gmail.com>
2024-04-09 18:47:31 +0200

    implementacion de la funcion actualizar
    Signed-off-by: Pau Rodrigo <paurodrigo7@gmail.com>

--- asignaturaentrega/src/asignaturasentrega/ModificadorDeTablas.java
+++ asignaturaentrega/src/asignaturasentrega/ModificadorDeTablas.java
@@ -143,6 +143,37 @@ public class ModificadorDeTablas {
 		frame.getContentPane().add(btnGuardar);
 		
 		JButton btnActualizar = new JButton("Actualizar");
+		btnActualizar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String nombre = txtNombre.getText();
+				String hora = txtHoras.getText();
+				String codigo = txtCodigo.getText();
+				if (nombre == null || nombre.isEmpty() ) {
+					JOptionPane.showMessageDialog(null, "Falta el nombre", "ERROR", JOptionPane.ERROR_MESSAGE);
+				
+				} else if (hora == null || hora.isEmpty() ) {
+					JOptionPane.showMessageDialog(null, "Falta las horas", "ERROR", JOptionPane.ERROR_MESSAGE);
+
+
+					
+				}else if (!txtCodigo.getText().isEmpty()) {
+					try {
+						Connection con = ConnectionSingleton.getConnection();
+						PreparedStatement ins_pstmt = con.prepareStatement("update asignatura set nombre=?,horas=? where id=?");
+						ins_pstmt.setString(1, nombre);
+						ins_pstmt.setInt(2, Integer.valueOf(hora));
+						ins_pstmt.setInt(3, Integer.valueOf(codigo));
+						ins_pstmt.executeUpdate();
+						JOptionPane.showMessageDialog(null, "Asignatura actualizada");
+						limpiar();
+						mostrar(model);
+					} catch (SQLException e1) {
+						e1.printStackTrace();
+					}
+				}
+				
+			}
+		});
 		btnActualizar.setBounds(339, 290, 117, 25);
 		frame.getContentPane().add(btnActualizar);
 		
